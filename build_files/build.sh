853f04c92be2104cf30093031c4dab7b6543bbfd
Christian Glombek <c.glombek@cosa.systems>
2025-10-27 20:47:49 +0100

    cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
-
-set -ouex pipefail
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-#dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-#systemctl enable podman.socket
-
-# # Asus/Surface for HWE
-# curl --retry 3 -Lo /etc/yum.repos.d/_copr_lukenukem-asus-linux.repo \
-#     https://copr.fedorainfracloud.org/coprs/lukenukem/asus-linux/repo/fedora-$(rpm -E %fedora)/lukenukem-asus-linux-fedora-$(rpm -E %fedora).repo
+set -xeuo pipefail
 
 # Remove Existing Kernel
 for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
@@ -42,50 +16,14 @@ cat /etc/dnf/dnf.conf
 cat /etc/yum.repos.d/*
 
 # Install Kernel
-dnf5 --setopt=disable_excludes=* -y install \
+dnf --setopt=disable_excludes=* -y install \
     /tmp/kernel-rpms/kernel-[0-9]*.rpm \
     /tmp/kernel-rpms/kernel-core-*.rpm \
     /tmp/kernel-rpms/kernel-modules-*.rpm
 
-# TODO: Figure out why akmods cache is pulling in akmods/kernel-devel
-#dnf5 -y install \
-#    /tmp/kernel-rpms/kernel-devel-*.rpm
+dnf versionlock add kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
-dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-
-#dnf5 -y install /tmp/akmods/kmods/*kvmfr*.rpm
-
-curl --retry 3 -Lo /etc/yum.repos.d/linux-surface.repo \
-        https://pkg.surfacelinux.com/fedora/linux-surface.repo
-
-# # Asus Firmware -- Investigate if everything has been upstreamed
-# # git clone https://gitlab.com/asus-linux/firmware.git --depth 1 /tmp/asus-firmware
-# # cp -rf /tmp/asus-firmware/* /usr/lib/firmware/
-# # rm -rf /tmp/asus-firmware
-
-# ASUS_PACKAGES=(
-#     asusctl
-#     asusctl-rog-gui
-# )
-
-SURFACE_PACKAGES=(
-    iptsd
-    libcamera
-    libcamera-tools
-    libcamera-gstreamer
-    libcamera-ipa
-    pipewire-plugin-libcamera
-)
-
-dnf5 -y install --skip-unavailable \
-    "${SURFACE_PACKAGES[@]}"
-
-dnf5 -y swap \
-    libwacom-data libwacom-surface-data
-
-dnf5 -y swap \
-    libwacom libwacom-surface
-
+# Configure surface kernel modules to load at boot
 tee /usr/lib/modules-load.d/ublue-surface.conf << EOF
 # Only on AMD models
 pinctrl_amd
@@ -119,10 +57,43 @@ surface_hid_core
 # Surface Laptop 3/Surface Book 3 and later
 surface_hid
 surface_kbd
+
 EOF
 
-KERNEL_SUFFIX=""
+# Install surface packages
+SURFACE_PACKAGES=(
+    iptsd
+)
+dnf config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+dnf config-manager setopt linux-surface.enabled=0
+dnf install -y --enablerepo="linux-surface" \
+    "${SURFACE_PACKAGES[@]}"
+dnf swap -y --enablerepo="linux-surface" \
+    libwacom-data libwacom-surface-data
+dnf swap -y --enablerepo="linux-surface" \
+    libwacom libwacom-surface
 
+# Install additional fedora packages
+ADDITIONAL_FEDORA_PACKAGES=(
+    #libcamera
+    #libcamera-tools
+    #libcamera-gstreamer
+    #libcamera-ipa
+    #pipewire-plugin-libcamera
+    #pipewire-v4l2
+    nextcloud-client-nautilus
+    firefox
+    calls
+    feedbackd
+    gnome-network-displays
+)
+
+dnf -y install --skip-unavailable \
+    "${SURFACE_PACKAGES[@]}"
+
+# Regenerate initramfs
+KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+export DRACUT_NO_XATTR=1
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
